563ace4791110258547c2f3bad02be8569188765
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-22 18:05:37 -0300

    Cria exercicio3.js

--- bloco_4/dia_1/exercicio2.js
+++ bloco_4/dia_1/exercicio2.js
@@ -1,8 +1,10 @@
-let num1 = 96;
-let num2 = 73;
+let num1 = 37;
+let num2 = 99;
 
 if(num1 > num2) {
     console.log(num1);
-} else {
+} else if (num2 > num1){
     console.log(num2);
+} else {
+    console.log("Entradas inválidas")
 }
