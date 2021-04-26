e9dfab2c83c666f1e430e54a2d60027dbdac9323
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-26 20:08:24 -0300

    Corrige  exercicio3.js

--- bloco_4/dia_3/Exercicio3.js
+++ bloco_4/dia_3/Exercicio3.js
@@ -1,6 +1,25 @@
-let numero = 5;
+let numero = 7;
 
-for (let linha = 1; linha <= numero; linha += 1) {
-    console.log(' '.repeat(numero - linha) + '*'.repeat(linha))
-}
+//for (let linha = 1; linha <= numero; linha += 1) {
+//    console.log(' '.repeat(numero - linha) + '*'.repeat(linha))
+//}
+
+let simbolo = '*';
+let vazio = ' '
+let espacos = (numero - 1)
+
+for (let index = 0; index < numero ; index +=1) {
+    let linha = ''
+    for (let asteriscos = 1; asteriscos <= numero; asteriscos +=1) {
+        if (asteriscos > espacos) {
+            linha = linha + simbolo;
+        } else {
+            linha = linha + vazio;
+        };  
+    };
+    console.log(linha)
+    espacos -= 1;
+};
+
+
 
