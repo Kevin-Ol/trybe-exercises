bcbeeac990e3c461dae6fba1d3e9581c42ae1f94
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-26 17:07:13 -0300

    Adiciona comandos horizontal, vertical e diagonal superior direita

---
+++ bloco_4/dia_3/queen_attack.js
@@ -0,0 +1,29 @@
+//posicao da rainha
+let posicaoRainhaLinha = 4;
+let posicaoRainhaColuna = 4;
+
+//posicao peca alvo
+let posicaoPecaLinha = 5;
+let posicaoPecaColuna = 7;
+
+let ataqueBemSucedido = false;
+
+if (posicaoRainhaLinha === posicaoPecaLinha || posicaoRainhaColuna === posicaoPecaColuna) {
+    ataqueBemSucedido = true;
+};
+
+//Diagonal superior direita
+for (let supDir = 1 ; supDir < 8 ; supDir += 1) {
+    let linhaRainha = posicaoRainhaLinha + supDir;
+    let colunaRainha = posicaoRainhaColuna + supDir;
+
+    if (linhaRainha > 8 || colunaRainha > 8) {
+        break;
+    }
+
+    if ((posicaoPecaLinha === linhaRainha) && (posicaoPecaColuna === colunaRainha)) {
+        ataqueBemSucedido = true;
+    }
+}
+
+console.log(ataqueBemSucedido)
