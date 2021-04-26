9a61f0dab900318b2f9062ca47169a7cb12d00c8
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-26 17:33:58 -0300

    Adiciona todas as diagonais

--- bloco_4/dia_3/queen_attack.js
+++ bloco_4/dia_3/queen_attack.js
@@ -1,10 +1,10 @@
 //posicao da rainha
-let posicaoRainhaLinha = 4;
-let posicaoRainhaColuna = 4;
+let posicaoRainhaLinha = 8;
+let posicaoRainhaColuna = 8;
 
 //posicao peca alvo
-let posicaoPecaLinha = 5;
-let posicaoPecaColuna = 7;
+let posicaoPecaLinha = 8;
+let posicaoPecaColuna = 4;
 
 let ataqueBemSucedido = false;
 
@@ -19,11 +19,53 @@ for (let supDir = 1 ; supDir < 8 ; supDir += 1) {
 
     if (linhaRainha > 8 || colunaRainha > 8) {
         break;
-    }
+    };
 
     if ((posicaoPecaLinha === linhaRainha) && (posicaoPecaColuna === colunaRainha)) {
         ataqueBemSucedido = true;
-    }
-}
+    };
+};
 
-console.log(ataqueBemSucedido)
+//Diagonal superior esquerda
+for (let supEsq = 1 ; supEsq < 8 ; supEsq += 1) {
+    let linhaRainha = posicaoRainhaLinha + supEsq;
+    let colunaRainha = posicaoRainhaColuna - supEsq;
+
+    if (linhaRainha > 8 || colunaRainha < 1) {
+        break;
+    };
+
+    if ((posicaoPecaLinha === linhaRainha) && (posicaoPecaColuna === colunaRainha)) {
+        ataqueBemSucedido = true;
+    };
+};
+
+//Diagonal inferior esquerda
+for (let infEsq = 1 ; infEsq < 8 ; infEsq += 1) {
+    let linhaRainha = posicaoRainhaLinha - infEsq;
+    let colunaRainha = posicaoRainhaColuna - infEsq;
+
+    if (linhaRainha < 1 || colunaRainha < 1) {
+        break;
+    };
+
+    if ((posicaoPecaLinha === linhaRainha) && (posicaoPecaColuna === colunaRainha)) {
+        ataqueBemSucedido = true;
+    };
+};
+
+//Diagonal inferior direita
+for (let infDir = 1 ; infDir < 8 ; infDir += 1) {
+    let linhaRainha = posicaoRainhaLinha - infDir;
+    let colunaRainha = posicaoRainhaColuna + infDir;
+
+    if (linhaRainha < 1 || colunaRainha > 8) {
+        break;
+    };
+
+    if ((posicaoPecaLinha === linhaRainha) && (posicaoPecaColuna === colunaRainha)) {
+        ataqueBemSucedido = true;
+    };
+};
+
+console.log(ataqueBemSucedido);
