c2d43da98d38066efd3eb98f29cea226d7c908bd
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-26 21:20:03 -0300

    Altera exercicio4.js

--- bloco_4/dia_3/Exercicio4.js
+++ bloco_4/dia_3/Exercicio4.js
@@ -1,6 +1,18 @@
-let numero = 9;
+let numero = 7;
 
-for (let linha = 0; linha < (numero - 2); linha += 1) {
-    console.log(' '.repeat(numero - linha - 3) + '*'.repeat(1 + 2 * linha ) + ' '.repeat(numero - linha - 3))
+//for (let linha = 0; linha < (numero - 2); linha += 1) {
+//    console.log(' '.repeat(numero - linha - 3) + '*'.repeat(1 + 2 * linha ) + ' '.repeat////(numero - linha - 3))
+//}
+
+let linha = ''
+let espaco = ' '
+let asterisco = '*'
+
+for (let index = 1; index <= numero; index += 1) {
+    if (index === ((numero + 1) / 2)) {
+        linha = linha + asterisco
+    } else {
+        linha = linha + espaco
+    }
 }
-
+console.log(linha)
