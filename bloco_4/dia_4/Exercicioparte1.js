e85b609663c11ea142e2fc30333b80afb90250c8
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-27 18:53:38 -0300

    Resolve parte 1 exercicio 4 e 5

--- bloco_4/dia_4/Exercicioparte1.js
+++ bloco_4/dia_4/Exercicioparte1.js
@@ -10,6 +10,25 @@ info['recorrente'] = 'sim';
 
 //console.log(info)
 
-for (let key in info) {
-    console.log(key)
+// for (let key in info) {
+//     console.log(key)
+// };
+
+// for (let key in info) {
+//     console.log(info[key])
+// };
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'sim'
+  };
+
+for (let key in info){
+    if (info[key] === info2[key] && info[key] === 'sim' && info2[key] === 'sim'){
+        console.log('Ambos recorrentes')
+    } else {
+        console.log(info[key] + ' e ' + info2[key])
+    };
 };
