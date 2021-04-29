f7ccf8816a7b4540a16fe7f7387e852ae6f9f618
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-29 17:21:46 -0300

    Modifica cript.js

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,8 @@
+document.getElementById('header-container').style.backgroundColor = '#61D747'
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink'
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow'
+document.querySelectorAll('.emergency-tasks h3 ')[0].style.backgroundColor = 'purple'
+document.querySelectorAll('.emergency-tasks h3 ')[1].style.backgroundColor = 'purple'
+document.querySelectorAll('.no-emergency-tasks h3 ')[0].style.backgroundColor = 'black'
+document.querySelectorAll('.no-emergency-tasks h3 ')[1].style.backgroundColor = 'black'
+document.getElementsByTagName('footer')[0].style.backgroundColor = 'green'
