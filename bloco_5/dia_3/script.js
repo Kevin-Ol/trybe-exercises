4b8707002e7a25ba5c05435a2e61ca4c8b4001b3
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-03 18:53:58 -0300

    Adiciona dias do mes e suas classes

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -14,3 +14,25 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+function createDaysOfTheMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    let monthDays = document.querySelector('#days');
+    for (let day of dezDaysList) {
+        let monthDayItem = document.createElement('li');
+        monthDayItem.innerHTML = day;
+
+        if (day === 24 || day === 31) {
+            monthDayItem.className = 'day holiday';
+        } else if (day === 4 || day === 11 || day === 18) {
+            monthDayItem.className = 'day friday';
+        } else if (day === 25) {
+            monthDayItem.className = 'day holiday friday';
+        } else {
+            monthDayItem.className = 'day';
+        }
+
+        monthDays.appendChild(monthDayItem);
+    }
+}
+
+createDaysOfTheMonth();
