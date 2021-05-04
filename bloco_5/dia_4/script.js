a9fe1f72bb9b895ddab97e174b231b8e72064a19
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-04 20:50:21 -0300

    Adiciona eventos para mudar cor do texto

--- bloco_5/dia_4/script.js
+++ bloco_5/dia_4/script.js
@@ -1,5 +1,3 @@
-
-
 let colors = ['black', 'white', 'blue', 'red', 'yellow', 'grey']
 
 for (let color = 0; color < colors.length ; color +=1) {
@@ -25,8 +23,31 @@ for (let color = 0; color < colors.length ; color +=1) {
         event.target.style.height = '30px';
         event.target.style.width = '30px';
     })
-
 }
 
+for (let color = 0; color < colors.length ; color +=1) {
+    let circle = document.createElement('div');
+    circle.style.backgroundColor = colors[color];
+    circle.style.height = '30px';
+    circle.style.width = '30px';
+    circle.style.border = '1px solid black';
+    circle.style.borderRadius = '100%';
+    circle.style.display = 'inline-block';
+    circle.style.verticalAlign = 'middle';
+    circle.style.marginLeft = '10px';
+    document.querySelector('.textColor').appendChild(circle);
+    circle.addEventListener('click', function () {
+        let change = circle.style.backgroundColor
+        document.body.style.color = change
+    })
+    circle.addEventListener('mouseenter', function (event) {
+        event.target.style.height = '45px';
+        event.target.style.width = '45px';
+    })
+    circle.addEventListener('mouseleave', function (event) {
+        event.target.style.height = '30px';
+        event.target.style.width = '30px';
+    })
+}
 
 
