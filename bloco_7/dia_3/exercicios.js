db12734592d0778ae64df04a6f19264458120d15
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-21 20:16:27 -0300

    Finaliza exercicios parte 2

--- bloco_7/dia_3/exercicios.js
+++ bloco_7/dia_3/exercicios.js
@@ -119,20 +119,56 @@ const addOne = (array) => {
 // assert.strictEqual(typeof addOne, 'function');
 // assert.deepStrictEqual(output, expected);
 // assert.deepStrictEqual(myArray, unchanged);
-///////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////
+// // const assert = require('assert');
+// // escreva a função wordLengths aqui
+// const wordLengths = (array) => {
+//     let result = [];
+//     for (let index in array) {
+//         result.push(array[index].length);
+//     }
+//     return result;
+// }
+
+// const words = ['sun', 'potato', 'roundabout', 'pizza'];
+// const expected = [3, 6, 10, 5];
+
+// assert.strictEqual(typeof wordLengths, 'function');
+// const output = wordLengths(words);
+// assert.deepStrictEqual(output, expected);
+////////////////////////////////////////////////////////
 // const assert = require('assert');
-// escreva a função wordLengths aqui
-const wordLengths = (array) => {
-    let result = [];
-    for (let index in array) {
-        result.push(array[index].length);
-    }
-    return result;
-}
+// escreva a função sumAllNumbers aqui
+// const sumAllNumbers = (array) => {
+//     let sum = 0;
+//     for (let index of array) {
+//         sum += index;
+//     }
+//     return sum;
+// }
 
-const words = ['sun', 'potato', 'roundabout', 'pizza'];
-const expected = [3, 6, 10, 5];
+// const numbers = [9, 23, 10, 3, 8];
+// const expected = 53;
+// const output = sumAllNumbers(numbers);
 
-assert.strictEqual(typeof wordLengths, 'function');
-const output = wordLengths(words);
-assert.deepStrictEqual(output, expected);
+// assert.strictEqual(typeof sumAllNumbers, 'function');
+// assert.strictEqual(output, expected);
+//////////////////////////////////////////////////////////
+// const assert = require('assert');
+// escreva a função findTheNeedle aqui
+const findTheNeedle = (array, word) => array.indexOf(word)
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
