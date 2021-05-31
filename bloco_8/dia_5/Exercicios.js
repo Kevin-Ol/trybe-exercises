a927779e60a7040394f258482ab2b298b7c35a95
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-31 18:30:58 -0300

    Finaliza exercicio 2

--- bloco_8/dia_5/Exercicios.js
+++ bloco_8/dia_5/Exercicios.js
@@ -10,3 +10,14 @@ const rectangles = [rectangle1, rectangle2, rectangle3];
 rectangles.forEach((rectangle) => {
   assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
 });
+
+// const assert = require('assert');
+
+// escreva sum abaixo
+const sum = (...numbers) => numbers.reduce((acc, current) => acc += current, 0)
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
