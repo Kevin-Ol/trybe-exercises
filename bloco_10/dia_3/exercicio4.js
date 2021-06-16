139fd18dd14b7b28fe6a3c316ae9bdb341d3f2f2
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-16 20:36:32 -0300

    4 a 6

---
+++ bloco_10/dia_3/exercicio4.js
@@ -0,0 +1,17 @@
+const upperCase = (str) => {
+    return str.toUpperCase()
+}
+
+const firstLetter = (str) => {
+    return str[0]
+}
+
+const joinStrings = (str1, str2) => {
+    return str1 + str2
+}
+
+module.exports = {
+    upperCase,
+    firstLetter,
+    joinStrings,
+}
