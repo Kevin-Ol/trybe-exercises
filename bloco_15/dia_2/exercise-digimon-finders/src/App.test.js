e01474a060e8669b7aab5fac70434b879b8836f1
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-19 23:11:19 -0300

    Finaliza exercicio do dia faltando cobrir "if"

--- bloco_15/dia_2/exercise-digimon-finders/src/App.test.js
+++ bloco_15/dia_2/exercise-digimon-finders/src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 
 describe('Teste da aplicação toda', () => {
 
-  it('renders App', async () => {
+  it('Testa resposta correta da api', async () => {
     render(<App />);
 
     const response = [
@@ -26,8 +26,12 @@ describe('Teste da aplicação toda', () => {
     userEvent.type(input, 'angemon');
     expect(input).toHaveValue('angemon');
 
+    const url = 'https://digimon-api.vercel.app/api/digimon/name/angemon';
+
     const button = screen.getByRole('button', 'Search Digimon');
     userEvent.click(button);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(url);
 
     const name = await screen.findByTestId('digimonName');
     expect(name).toHaveTextContent(response[0].name);
@@ -37,6 +41,35 @@ describe('Teste da aplicação toda', () => {
 
     const img = await screen.findByRole('img');
     expect(img).toHaveProperty('src', response[0].img);
+  });
+
+  it('Testa resposta errada da api', async () => {
+    render(<App />);
+
+    const response = 
+      {"ErrorMsg":"Pikachu is not a Digimon in our database."}
+
+    global.fetch = jest.fn()
+      .mockResolvedValue({json: () => Promise.resolve(response)})
+    
+    const linkElement = screen.getByText(/Faça uma pesquisa/i);
+    expect(linkElement).toBeInTheDocument();
+
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveValue('');
+
+    userEvent.type(input, 'pikachu');
+    expect(input).toHaveValue('pikachu');
+
+    const url = 'https://digimon-api.vercel.app/api/digimon/name/pikachu';
+
+    const button = screen.getByRole('button', 'Search Digimon');
+    userEvent.click(button);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(url);
+
+    const error = await screen.findByText(response.ErrorMsg);
+    expect(error).toBeInTheDocument();
   });
 
 });
