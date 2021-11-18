234ae1ee27f49f6fb5743bccd9b705b666b6020c
Kevin Oliveira <kevin.zero@hotmail.com>
2021-11-18 09:29:59 -0300

    Acrescenta rota movies/:id

--- bloco_27/dia_4/tdd_exercise/src/index.js
+++ bloco_27/dia_4/tdd_exercise/src/index.js
@@ -11,6 +11,8 @@ app.get('/movies', MovieController.getAll);
 
 app.post('/movies', MovieController.create);
 
+app.post('/movies/:id', MovieController.getById);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
